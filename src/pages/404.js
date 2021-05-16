a5c07536f1d6a819514046e66e7326346383c62b
Nassim Eddequiouaq <eddequiouaq.nassim@gmail.com>
2021-05-16 13:07:35 -0700

    Switch to Ghost
    Signed-off-by: Nassim Eddequiouaq <eddequiouaq.nassim@gmail.com>

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,18 @@
-import * as React from "react"
-
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import React from 'react'
+import { Link } from 'gatsby'
+import { Layout } from '../components/common'
 
 const NotFoundPage = () => (
-  <Layout>
-    <Seo title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+    <Layout>
+        <div className="container">
+            <article className="content" style={{ textAlign: `center` }}>
+                <h1 className="content-title">Error 404</h1>
+                <section className="content-body">
+                    Page not found, <Link to="/">return home</Link> to start over
+                </section>
+            </article>
+        </div>
+    </Layout>
 )
 
 export default NotFoundPage
